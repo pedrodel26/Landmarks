b69ab3b8de940e2b15cd4d71ea854bc64a034faf
pedrodel26 <pedromodesto26@gmail.com>
2024-03-07 00:21:13 -0300

    Updated.

--- Landmarks/FeatureCard.swift
+++ Landmarks/FeatureCard.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct FeatureCard: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        landmark.featureImage?
+            .resizable()
+            .overlay {
+                TextOverlay(landmark: landmark)
+            }
     }
 }
 
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var gradient: LinearGradient {
+        .linearGradient(
+            Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            gradient
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+                
+            }
+            .foregroundStyle(.white)
+        }
+    }
+}
+
+
 #Preview {
-    FeatureCard()
+    FeatureCard(landmark: ModelData().features[0])
+        .aspectRatio(3 / 2, contentMode: .fit)
 }
